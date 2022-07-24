6c73abbd2c40e42f789e913a8f2c1f5bb54cf010
Matt Dittes <mattdittes@gmail.com>
2022-07-23 20:05:31 -0400

    commit tree 5383768a92ed21b652af187c2d590cafd046252b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+/* Write a function submitData
+    submitData takes two strings as arguments
+        one is user's name, other is user's email
+    POST request to local host URL using fetch()
+        includes destination URL
+        headers for Content-Type and Accept
+        body with name and email as arguments to submitData
+            'name' and 'email' are keys within an object
+                object should be stringified
+*/
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(res => res.json())
+    .then(object => document.body.innerHTML = object["id"])
+    .catch(error => document.body.innerHTML = error.message)
+};
+
